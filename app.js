632a829905cd7c0f20953e97a2fb26b8109ddf17
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2021-10-10 23:22:08 +0300

    Second-level sort added (sorting items with same subject by date), but only on the first page

--- app.js
+++ app.js
@@ -4,6 +4,53 @@ if(name) {
     name[0].innerHTML = "Вася Петькин";
 }
 
+function parseToRFC2822(x) {
+    let str = x.substring(4, x.length);
+    console.log(str);
+    a = str.split(" ")[1];
+    console.log(a);
+    switch (a) {
+        case "января":
+            str = str.replace("января", "January");
+            break;
+        case "февраля":
+            str = str.replace("февраля", "February");
+            break;
+        case "марта":
+            str = str.replace("марта", "March");
+            break;
+        case "апреля":
+            str = str.replace("апреля", "April");
+            break;
+        case "мая":
+            str = str.replace("мая", "May");
+            break;
+        case "июня":
+            str.replace("июня", "June");
+            break;
+        case "июля":
+            str = str.replace("июля", "July");
+            break;
+        case "августа":
+            str = str.replace("августа", "August");
+            break;
+        case "сентября":
+            str = str.replace("сентября", "September");
+            break;
+        case "октября":
+            str = str.replace("октября", "October");
+            break;
+        case "ноября":
+            str = str.replace("ноября", "November");
+            break;
+        case "декабря":
+            str = str.replace("декабря", "December");
+            break;
+    }
+    str = str.split("—")[0];
+    return str;
+}
+
 if(document.location.toString().indexOf("home.mephi.ru/lesson_videos/") > 0) {
     let nodeList = document.querySelectorAll(".list-group-item");
     let itemsArray = [];
@@ -22,6 +69,14 @@ if(document.location.toString().indexOf("home.mephi.ru/lesson_videos/") > 0) {
         }
         if(textA > textB) return 1;
         if(textA < textB) return -1;
+        let stringDateA = nodeA.querySelector("h4").innerText.split("\n")[2].trim();
+        let stringDateB = nodeB.querySelector("h4").innerText.split("\n")[2].trim();
+        let dateA = new Date();
+        let dateB = new Date();
+        dateA.setTime(Date.parse(parseToRFC2822(stringDateA)));
+        dateB.setTime(Date.parse(parseToRFC2822(stringDateB)));
+        if(dateA > dateB) return -1;
+        if(dateA < dateB) return 1;
         return 0;
     }).forEach(function (node) {
         parent.appendChild(node);
