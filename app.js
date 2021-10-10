8e694466c8ed2c08e653fbda4a8928a5c33dfdf3
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2021-10-10 22:11:07 +0300

    Lectures are sorted by subjects, but only on the first page

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const name = document.getElementsByClassName("hidden-xs");
 
 if(name) {
-    alert(name.length);
     name[0].innerHTML = "Вася Петькин";
+}
+
+if(document.location.toString().indexOf("home.mephi.ru/lesson_videos/") > 0) {
+    let nodeList = document.querySelectorAll(".list-group-item");
+    let itemsArray = [];
+    let parent = nodeList[0].parentNode;
+    for(let i = 0; i < nodeList.length; i++) {
+        itemsArray.push(parent.removeChild(nodeList[i]));
+    }
+    itemsArray.sort(function (nodeA, nodeB) {
+        let textA = nodeA.querySelector("span").textContent.split("\n")[2];
+        let textB = nodeB.querySelector("span").textContent.split("\n")[2];
+        if(textA === "") {
+            return 1;
+        }
+        if(textB === "") {
+            return -1;
+        }
+        if(textA > textB) return 1;
+        if(textA < textB) return -1;
+        return 0;
+    }).forEach(function (node) {
+        parent.appendChild(node);
+    })
 }
